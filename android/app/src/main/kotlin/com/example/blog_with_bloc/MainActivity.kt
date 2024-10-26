5b280f48baa02173bd5ec9327f9c4e6a4b5ae308
mohib <you@example.com>
2024-10-26 19:29:33 +0600

    commit

---
+++ android/app/src/main/kotlin/com/example/blog_with_bloc/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.blog_with_bloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = 'https://rdolksvapkhpzgdquknz.supabase.co';
+  static const supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkb2xrc3ZhcGtocHpnZHF1a256Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk3NzE5MDUsImV4cCI6MjA0NTM0NzkwNX0.UyM_u8rXS8wjyb4LhFP8wrcgX6e8E_8Ebh1qSq8K65M';
+}
